ef72ecf3f1805d7af0b92f5a06fe52bbd448167a
Renato Rosa Franco <renato_ny@live.com>
2017-12-18 14:58:25 -0200

    Contact model added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
+//= require boostrap
+//= require angular
 //= require turbolinks
 //= require_tree .
